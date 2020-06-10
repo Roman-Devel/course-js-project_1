1e96281d27dcf0196a7baedee6a1a272a3fc832b
Roman-Devel <melnikov.roman.lang@gmail.com>
2020-06-11 01:38:15 +0600

    if else__switch

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
-'use strict'; //пишем на стандарте ES6
+let num = 50;
 
-let money = prompt("Ваш бюджет на месяц?", ''),
-	time = prompt("Введите дату в формате YYYY-MM-DD", '');
+if (num < 49) {
+	console.log("Неверно!");
+} else if (num > 100) {
+	console.log("Много!");
+} else {
+	console.log("Верно!");
+}
 
-let appData = {
-	budget: money,
-	timeData: time,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	savings: false
-};
+(num == 50) ? console.log("Верно!") : console.log("Неверно!");
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
-
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-
-alert(appData.budget / 30);
+switch (num) {
+	case num < 49:
+		console.log("Неверно!");
+		break;
+	case num > 100:
+		console.log("Много!");
+		break;
+	case num > 80:
+		console.log("Все еще много!");
+		break;
+	case 50:
+		console.log("Верно!");
+		break;
+	default:
+		console.log("Что-то пошло не так!");
+		break;
+}
