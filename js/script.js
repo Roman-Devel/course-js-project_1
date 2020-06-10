a336555edbb663ea0dae6eae872f8bf41e5391a4
Roman-Devel <melnikov.roman.lang@gmail.com>
2020-06-11 02:15:29 +0600

    cycles-while__do_while__for

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-let num = 50;
-
 if (num < 49) {
 	console.log("Неверно!");
 } else if (num > 100) {
@@ -26,4 +24,24 @@ switch (num) {
 	default:
 		console.log("Что-то пошло не так!");
 		break;
+}
+
+let num = 50;
+
+while (num < 55) {
+	console.log(num);
+	num++;
+}
+
+do {
+	console.log(num);
+	num++;
+}
+while (num < 55)
+
+for (let i = 1; i < 8; i++) {
+	if (i == 6) {
+		continue;
+	}
+	console.log(i);
 }
