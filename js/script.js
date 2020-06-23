ddfff5d23b9de6a588bd83fbd06c963ec0127690
Roman-Devel <melnikov.roman.lang@gmail.com>
2020-06-23 17:54:53 +0600

    mobileEvents

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
-let btn = document.querySelectorAll('button'),
-	wrap = document.querySelector('.wrapper'),
-	link = document.querySelector('a');
+// touchstart 			--- касание к элементу (таб на элементе)
+// touchmove 			--- перетаскивание элемента
+// touchend 			--- когда убираем палец с элемента
+// touchenter 			--- когда водим пальцем по экрану и заходим на определенный элемент
+// touchleave			--- когда палец покидает пределы элемента
+// touchcancel			--- когда палец не регистрируется внутри браузера (выходим за его пределы)
 
-// btn[0].onclick = function() {							// --- событие клика на элемент
-// 	alert('Вы нажали первую кнопку');
-// };
+window.addEventListener('DOMContentLoaded', function() {
+	let box = document.querySelector('.box');
 
-// btn[0].addEventListener('click', function(event) { 			// --- событие клика через callBack функцию
-// 	console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
-// });
+	// box.addEventListener('touchstart', function(event) {
+	// 	event.preventDefault();
+	// 	console.log('Red Box: touchstart');
+	// 	console.log(event.target);										// --- вывод элемента, на котором было касание
+	// 	console.log(event.touches[0].target);							// --- вывод элемента, на котором было касание от первого пальца (мобильное событие)
+	// 	console.log(event.touches);										// --- регистрация всех пальцев, которые были использованы в событии
+	// 	console.log(event.changedTouches);								// --- аналогичное предыдущему свойству
+	// 	console.log(event.targetTouches);								// --- регистрация только тех пальцев, которые взаимодействуют с элементом
+	// });
 
-// wrap.addEventListener('click', function(event) {
-// 	console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
-// });
-
-link.addEventListener('click', function(event) {
-	event.preventDefault();
-	console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
-});
-
-btn.forEach(function(item) {
-	item.addEventListener('mouseleave', function() { 		// --- когда мышь выходит за пределы элемента
-		console.log('Вышли!');
+	box.addEventListener('touchmove', function(event) {
+		event.preventDefault();
+		console.log('Red Box: ' + event.touches[0].pageX);				// --- получаем координаты оси Х от первого пальца
 	});
-});
 
-// btn[0].addEventListener('mouseenter', function() {		// --- событие наведения мышки на элемент
-// 	alert('Вы навели на первую кнопку');
-// });
+	// box.addEventListener('touchend', function(event) {
+	// 	event.preventDefault();
+	// 	console.log('Red Box: touchend');
+	// });
+});
