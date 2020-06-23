f01accc92bfcfe81a1d9963f4421591b086b8991
Roman-Devel <melnikov.roman.lang@gmail.com>
2020-06-23 17:56:40 +0600

    regularExpression

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
-// touchstart 			--- касание к элементу (таб на элементе)
-// touchmove 			--- перетаскивание элемента
-// touchend 			--- когда убираем палец с элемента
-// touchenter 			--- когда водим пальцем по экрану и заходим на определенный элемент
-// touchleave			--- когда палец покидает пределы элемента
-// touchcancel			--- когда палец не регистрируется внутри браузера (выходим за его пределы)
-
 window.addEventListener('DOMContentLoaded', function() {
-	let box = document.querySelector('.box');
 
-	// box.addEventListener('touchstart', function(event) {
-	// 	event.preventDefault();
-	// 	console.log('Red Box: touchstart');
-	// 	console.log(event.target);										// --- вывод элемента, на котором было касание
-	// 	console.log(event.touches[0].target);							// --- вывод элемента, на котором было касание от первого пальца (мобильное событие)
-	// 	console.log(event.touches);										// --- регистрация всех пальцев, которые были использованы в событии
-	// 	console.log(event.changedTouches);								// --- аналогичное предыдущему свойству
-	// 	console.log(event.targetTouches);								// --- регистрация только тех пальцев, которые взаимодействуют с элементом
-	// });
+	// new RegExp('pattern', 'flags'); 									// --- первый способ создания Регулярного Выражения (паттерн - шаблон и флаги)
+	// /pattern/														// --- второй способ создания Регулярного Выражения
 
-	box.addEventListener('touchmove', function(event) {
-		event.preventDefault();
-		console.log('Red Box: ' + event.touches[0].pageX);				// --- получаем координаты оси Х от первого пальца
-	});
+	// let ans = prompt('Введите ваше имя', '');
 
-	// box.addEventListener('touchend', function(event) {
-	// 	event.preventDefault();
-	// 	console.log('Red Box: touchend');
-	// });
+	// let reg = /n/gi;													// --- паттерн n, и 2 флага (глобальность и регистр)
+	// console.log(reg.test(ans));											// --- тестирует строку на вхождения и выводит Boolean
+
+	// console.log(ans.search(reg));
+	// --- метод search() ищет только первое вхождение по паттерну, если находит, тогда выводит позицию, если нет, возвращает -1
+	// // console.log(ans.match(reg));										// --- получаем совпадение в виде массива
+
+	// флаги:
+	// i --- поиск чего-то вне зависимости от регистра
+	// g --- глобальность (ищет не только первое вхождение, но и все остальные)
+	// m --- многострочность
+
+	// классы:
+	// \d --- поиск цифр		\D --- ищем не цифры
+	// \w --- поиск букв		\W --- ищем не буквы
+	// \s --- поиск пробелов	\S --- ищем не пробелы
+
+	// let pass = prompt('Введите пароль', '');
+
+	// console.log(pass.replace(/./g, "*"));								// --- заменяем все символы глобально на знак *
+	// alert('12-34-56'.replace(/-/g, ":"));								// --- заменяем все минусы глобально на знак :
+
+	// let ans = prompt('Введите число', '');
+	// let reg = /\d/g;														// --- глобально ищем все цифры в строке
+	// console.log(ans.match(reg));	
+
+	let str = 'My name is/ R2D2';
+	console.log(str.match(/\w\d\w\d/i));									// --- ищем букву, цифру, букву, цифру без регистра
+	console.log(str.match(/\//i));											// --- ищем спец. символ типа слеша(/) или * через экранирование (\/), (\*)
+
 });
