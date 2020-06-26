62f6e359fd98f80d20f8e2ae74804b2fed4fe5a9
Roman-Devel <melnikov.roman.lang@gmail.com>
2020-06-26 18:49:46 +0600

    functions_constructors

--- js/script.js
+++ js/script.js
@@ -1,46 +1,24 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-// let timerId = setTimeout(sayHello, 3000);			// --- функция срабатывает 1 раз с задержкой (функция, задержка в миллисек)
-// clearTimeout(timerId);								// --- очистка таймера
-
-// let timerId = setInterval(sayHello, 3000);			// --- интервальный повтор функции (функция, задержка в миллисек)
-// clearTimeout(timerId);								// --- очистка таймера
-
-// function sayHello() {
-// 	console.log('Hello World!');
-// }
-
-// let timerId = setTimeout(function log() {			// --- рекурсивный вызов setTimeout - функция вызывает сама себя и работает как setInterval
-// 	console.log('Hello!');								// нужен в тех случаях, когда внутренняя функция отрабатывает дольше, чем заданная задержка
-// 	setTimeout(log, 2000);
-// });
-
-let btn = document.querySelector('.btn'),
-	elem = document.querySelector('.box');
-
-function myAnimation() {
-	let pos = 0;
-	let id = setInterval(frame, 10);
-
-	function frame() {
-		if (pos == 300) {
-			clearInterval(id);
-		} else {
-			pos++;
-			elem.style.top = pos + 'px';
-			elem.style.left = pos + 'px';
-		}
+	// --- функция конструктор, предназначена для конструирования объектов и создания множества подобных копий, return (возврат) не нужен --- \\
+	function User(name, id) {
+		this.name = name;
+		this.id = id;
+		this.human = true;
+		this.hello = function() {
+			console.log('Hello! ' + this.name);
+		};
 	}
-}
-
-btn.addEventListener('click', myAnimation);
-
-let btnBlock = document.querySelector('.btn-block');
-
-btnBlock.addEventListener('click', function(event) {
-	if (event.target && event.target.matches('button.first')) {
-		console.log("Hello!");
+	User.prototype.exit = function(name) {
+		console.log('Пользователь ' + this.name + ' ушел');
 	}
-});
+
+	let ivan = new User('Ivan', 25),
+		alex = new User('Alex',20);
+
+	console.log(ivan);
+	console.log(alex);
+	
+	ivan.exit();
 
 });
