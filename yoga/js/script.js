36b2ade8e845a49dc0954e31c9957062fbd329a2
Roman-Devel <melnikov.roman.lang@gmail.com>
2020-06-28 23:06:00 +0600

    MyMainProject__ModalWindow

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -85,4 +85,22 @@ window.addEventListener('DOMContentLoaded', function() {
 	}
 	setClock('timer', deadline);										// --- 1 аргумент (id блока с таймером в верстке), 2 аргумент (конечная дата)
 
+	// ---------- Реализация Modal Window ---------- \\
+	// --------------------------------------------- \\
+	let more = document.querySelector('.more'),							// --- кнопка узнать больше
+		overlay = document.querySelector('.overlay'),					// --- блок с модальным окном
+		close = document.querySelector('.popup-close');					// --- крестик
+	
+	more.addEventListener('click', function() {
+		overlay.style.display = 'block';								// --- показываем модальное окно
+		this.classList.add('more-splash');								// --- добавляем кнопке класс с анимацией
+		document.body.style.overflow = 'hidden';						// --- запрещаем прокрутку страницы
+	});
+
+	close.addEventListener('click', function() {
+		overlay.style.display = 'none';									// --- скрываем модальное окно
+		more.classList.remove('more-splash');							// --- удаляем у кнопки класс с анимацией
+		document.body.style.overflow = '';								// --- снова разрешаем прокрутку страницы
+	});
+
 });
