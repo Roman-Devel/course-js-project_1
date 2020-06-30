d746dd50582eea051ef2a642c4828450d98263ae
Roman-Devel <melnikov.roman.lang@gmail.com>
2020-06-30 23:04:23 +0600

    JSON

--- js/script.js
+++ js/script.js
@@ -1,84 +1,11 @@
-window.addEventListener('DOMContentLoaded', function() {
+let options = {
+	width: 1366,
+	height: 768,
+	background: 'red',
+	font: {
+		size: '16px',
+		color: '#fff'
+	}
+};
 
-// --- функция конструктор, предназначена для конструирования объектов и создания множества подобных копий, return (возврат) не нужен --- \\
-	// function User(name, id) {
-	// 	this.name = name;
-	// 	this.id = id;
-	// 	this.human = true;
-	// 	this.hello = function() {
-	// 		console.log('Hello! ' + this.name);
-	// 	};
-	// }
-	// User.prototype.exit = function(name) {
-	// 	console.log('Пользователь ' + this.name + ' ушел');
-	// }
-
-	// let ivan = new User('Ivan', 25),
-	// 	alex = new User('Alex',20);
-
-	// console.log(ivan);
-	// console.log(alex);
-	
-	// ivan.exit();
-
-	// function showThis(a, b) {
-	// 	console.log(this);
-	// 	function sum() {
-	// 		console.log(this);
-	// 		return a + b;
-	// 	}
-	// 	console.log(sum());
-	// }
-	// showThis(4, 5);
-	// showThis(5, 5);
-
-	// let obj = {
-	// 	a: 20,
-	// 	b: 15,
-	// 	sum: function() {
-	// 		console.log(this);
-	// 		function shout() {
-	// 			console.log(this);
-	// 		}
-	// 		shout();
-	// 	}
-	// };
-	// obj.sum();
-
-	// let user = {
-	// 	name: 'John'
-	// };
-
-	// function sayName(surname) {
-	// 	console.log(this);
-	// 	console.log(this.name + ' ' + surname);
-	// }
-
-	// console.log(sayName.call(user, 'Smith'));		// --- привязываем функцию с объектом, передает строку
-	// console.log(sayName.apply(user, ['Snow']));		// --- привязываем функцию с объектом, может передать массив параметров
-
-	// function count(number) {
-	// 	return this * number;
-	// }
-
-	// let double = count.bind(2);						// --- метод bind жестко связывает значение в скобках с контекстом вызова this
-	// console.log(double(3));
-	// console.log(double(10));
-
-	let btn = document.querySelector('button');
-
-	btn.addEventListener('click', function() {
-		console.log(this);
-		this.style.backgroundColor = 'red';
-		function showThis() {
-			console.log(this);
-		}
-		showThis();
-	});
-
-	// 1) Просто вызов функции - window / undefined
-	// 2) Метод объекта - this = объект
-	// 3) Конструктор (new) - this = новый созданный объект
-	// 4) Указание конкретного контекста - call, apply, bind
-
-});
+console.log(JSON.parse(JSON.stringify(options)));					// --- преобразование в JSON формат передачи данных (stringify) и обратно (parse)
