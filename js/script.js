658c05c05acf10f16ddd28b893b9e78ba54c8dec
Roman-Devel <melnikov.roman.lang@gmail.com>
2020-07-05 20:53:57 +0600

    MyMainProject__sendForm

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
-let options = {
-	width: 1366,
-	height: 768,
-	background: 'red',
-	font: {
-		size: '16px',
-		color: '#fff'
-	}
-};
+// let options = {
+// 	width: 1366,
+// 	height: 768,
+// 	background: 'red',
+// 	font: {
+// 		size: '16px',
+// 		color: '#fff'
+// 	}
+// };
 
-console.log(JSON.parse(JSON.stringify(options)));					// --- преобразование в JSON формат передачи данных (stringify) и обратно (parse)
+// console.log(JSON.parse(JSON.stringify(options)));					// --- преобразование в JSON формат передачи данных (stringify) и обратно (parse)
+
+let inputRub = document.getElementById('rub'),
+	inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+	let request = new XMLHttpRequest();
+
+	// request.open(method, url, async, login, pass);
+	request.open('GET', 'js/current.json');
+	request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+	request.send();
+
+	//status --- состояние сервера, цифровой ответ - 404
+	//statusText --- текстовый ответ от сервера
+	//responseText / response --- текст ответа сервера
+	//readyState  --- текущее состояние запроса
+
+	request.addEventListener('readystatechange', function() {
+		if (request.readyState === 4 && request.status == 200) {
+			let data = JSON.parse(request.response);
+
+			inputUsd.value = inputRub.value / data.usd;
+		} else {
+			inputUsd.value = "Что-то пошло не так!";
+		}
+	});
+});
