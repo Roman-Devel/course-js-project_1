8f04108c84d12b7cca1343a477f86008644d3b2a
Roman-Devel <melnikov.roman.lang@gmail.com>
2020-07-10 20:41:55 +0600

    MyMainProject__Calculator

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -150,4 +150,102 @@ window.addEventListener('DOMContentLoaded', function() {
 		}
 	});
 
+	// ---------- Slider ---------- \\
+	// ---------------------------- \\
+	let slideIndex = 1,
+		slides = document.querySelectorAll('.slider-item'),
+		prev = document.querySelector('.prev'),
+		next = document.querySelector('.next'),
+		dotsWrap = document.querySelector('.slider-dots'),
+		dots = document.querySelectorAll('.dot');
+
+	showSlides(slideIndex);
+
+	function showSlides(n) {
+
+		if (n > slides.length) {										// после последнего слайда возвращаемся к самому первому
+			slideIndex = 1;
+		}
+		if (n < 1) {													// если перелистнуть с первого слайда назад, мы вернемся к последнему слайду
+			slideIndex = slides.length;
+		}
+
+		slides.forEach((item) => item.style.display = 'none');			// первый способ перебора псевдо-массива (скрываем все слайды)
+		// for (let i = 0; i < slides.length; i++) {					// второй способ перебора псевдо-массива (скрываем все слайды)
+		// 	slides[i].style.display = 'none';
+		// }
+		dots.forEach((item) => item.classList.remove('dot-active'));	// методом перебора удаляем активный класс из точек
+
+		slides[slideIndex - 1].style.display = 'block';					// показываем самый первый слайд
+		dots[slideIndex - 1].classList.add('dot-active');				// добавляем класс активности первой точке
+
+	}
+
+	function plusSlides(n) {											// увеличивает значение индекса слайда
+		showSlides(slideIndex += n);
+	}
+	function currentSlide(n) {											// значение основного значения слайда (корректного в данный момент)
+		showSlides(slideIndex = n);
+	}
+
+	prev.addEventListener('click', function() {							// слайд назад (по клику)
+		plusSlides(-1);
+	});
+
+	next.addEventListener('click', function() {							// слайд вперед (по клику)
+		plusSlides(1);
+	});
+
+	dotsWrap.addEventListener('click', function(event) {				// используем делегирование событий (нажатие на точки и переход по слайдам)
+		for (let i = 0; i < dots.length + 1; i++) {
+			if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+				currentSlide(i);
+			}
+		}
+	});
+
+	// ---------- Calculator ---------- \\
+	// -------------------------------- \\
+	let persons = document.querySelectorAll('.counter-block-input')[0],
+		restDays = document.querySelectorAll('.counter-block-input')[1],
+		place = document.getElementById('select'),
+		totalValue = document.getElementById('total'),
+		personsSum = 0,
+		daysSum = 0,
+		total = 0;
+
+	totalValue.innerHTML = 0;
+
+	persons.addEventListener('input', function() {						// событие следит за каждым введенным символом
+		personsSum = +this.value;										// получаем то, что ввел пользователь
+		total = (daysSum + personsSum) * 4000;							// наша математическая формула расчета
+
+		if (restDays.value == '' || persons.value == '') {				// если дни или количество человек не заполнены
+			totalValue.innerHTML = 0;									// тогда выводим в общей сумме 0
+		} else {
+			totalValue.innerHTML = total;								// иначе выводим сумму
+		}
+	});
+
+	restDays.addEventListener('input', function() {
+		daysSum = +this.value;
+		total = (daysSum + personsSum) * 4000;
+
+		if (restDays.value == '' || persons.value == '') {
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = total;
+		}
+	});
+
+	place.addEventListener('input', function() {
+		if (restDays.value == '' || persons.value == '') {
+			totalValue.innerHTML = 0;
+		} else {
+			let a = total;
+			totalValue.innerHTML = a * this.options[this.selectedIndex].value;	// используем свойство value из выпадающего списка
+		}
+	});
+
+
 });
