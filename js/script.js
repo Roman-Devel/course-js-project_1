406f3f88c28a0e7621b7553caa15d5700a0ab884
Roman-Devel <melnikov.roman.lang@gmail.com>
2020-07-12 18:15:58 +0600

    LocalStorage

--- js/script.js
+++ js/script.js
@@ -1,55 +1,7 @@
-// let options = {
-// 	width: 1366,
-// 	height: 768,
-// 	background: 'red',
-// 	font: {
-// 		size: '16px',
-// 		color: '#fff'
-// 	}
-// };
+localStorage.setItem('number', 1);						// создание хранилища данных (ключ, значение)
 
-// console.log(JSON.parse(JSON.stringify(options)));					// --- преобразование в JSON формат передачи данных (stringify) и обратно (parse)
+console.log(localStorage.getItem('number'));			// получить значение ключа
 
-let inputRub = document.getElementById('rub'),
-	inputUsd = document.getElementById('usd');
+localStorage.removeItem('number');						// удаление ключа
 
-inputRub.addEventListener('input', () => {
-
-function catchData() {
-
-	return new Promise(function(resolve, reject) {
-		let request = new XMLHttpRequest();
-		request.open('GET', 'js/current.json');
-		request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-		request.send();
-
-		request.onload = function() {
-			if(request.readyState === 4) {
-				if(request.status == 200) {
-					resolve(this.response)
-				}
-				else {
-					reject();
-				}
-			}
-		}
-	});
-};
-
-	// request.open(method, url, async, login, pass);
-
-	//status --- состояние сервера, цифровой ответ - 404
-	//statusText --- текстовый ответ от сервера
-	//responseText / response --- текст ответа сервера
-	//readyState  --- текущее состояние запроса
-
-catchData()
-		.then(response => {
-			console.log(response);
-			let data = JSON.parse(response);
-			inputUsd.value = inputRub.value / data.usd;
-		})
-		.then(() => console.log(5000))
-		.catch(() => inputUsd.value = "Что-то пошло не так!")
-
-});
+localStorage.clear();									// полное очищение хранилища
